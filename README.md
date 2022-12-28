b52af26ce0f488bc0df75a196845aa4bb12ce1d0
Subhayu Dutta <duttasuvo90@gmail.com>
2022-12-28 13:05:07 +0530

    second commit

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # diabetesprediction
 
-1. Git Clone
+1. git Clone
 2. git config --global user.name ""
     git config --global user.email ""
+3. git add .
+4. git status   
+5. git commit -m ""
+6. git push origin main
 Create new environment
 
 ```
---
+++ app.py
@@ -0,0 +1,42 @@
+import json
+import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+## Load the model
+diabeticpred=pickle.load(open('diabetespred.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output=diabeticpred.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=diabeticpred.predict(final_input)[0]
+    res=""
+    if output==0:
+        res="You will not have Diabetes"
+    else:
+        res="You will have Diabetes"
+    return render_template("index.html",prediction_text=res)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
